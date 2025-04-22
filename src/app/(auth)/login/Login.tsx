badb35b9a648bdfc45489d1b01ae263669e6b9bb
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-04-22 18:48:22 +0700

    [Task] 114982 - [330] [Develop] Mobile - Develop UI for Select Plan

--- src/app/(auth)/login/Login.tsx
+++ src/app/(auth)/login/Login.tsx
@@ -14,32 +14,28 @@ const Login = () => {
   };
 
   return (
-    <div
-      className='absolute left-0 top-0 -z-10 h-[100vh] w-full bg-cover bg-center'
-      style={{
-        backgroundImage: "url('/login_backg.svg')",
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      <div className='relative z-10 mt-[445px] p-6'>
-        <div className='justify-self-center text-primaryBlue'>
-          Get an instant quote with Myinfo login
+    <div className='relative min-h-[100svh]'>
+      <img
+        className='h-full w-full object-cover'
+        src='/login_bg_top.svg'
+        alt='Background Image'
+      />
+      <div className='text-center text-primaryBlue'>
+        Get an instant quote with Myinfo login
+      </div>
+      <div className='relative z-10 mx-auto max-w-md px-4'>
+        <div className='flex items-center gap-2 rounded-lg bg-white px-4 py-3 shadow-lg shadow-black/20'>
+          <p className='text-xl font-semibold'>Retrieve Myinfo with</p>
+          <Image
+            src='/singpass.svg'
+            alt='Logo'
+            width={100}
+            height={100}
+            className='pt-2'
+          />
         </div>
-
-        <div className='mb-6 mt-6 rounded-lg bg-white p-4 shadow-lg shadow-black/20'>
-          <div className='flex items-center justify-center'>
-            <div className='flex flex-row flex-wrap items-center justify-center gap-x-2 gap-y-1 text-center'>
-              <h1 className='whitespace-nowrap font-bold'>
-                Retrieve Myinfo with
-              </h1>
-              <Image src='/singpass.svg' alt='Logo' width={100} height={100} />
-            </div>
-          </div>
-        </div>
-
-        <div className='flex items-center justify-center'>
-          <div className='mr-[4px]'>or,</div>
+        <div className='flex items-center justify-center gap-1 text-sm'>
+          <span>or,</span>
           <LinkButton
             type='link'
             className='pl-0'
@@ -48,17 +44,13 @@ const Login = () => {
             continue without Myinfo login
           </LinkButton>
         </div>
-
-        <div className='mt-4 flex items-center justify-center'>
-          <Checkbox className='custom-checkbox mr-[4px]' />
-          <div className='mr-[4px]'>
-            By using this platform, you agree to our
-          </div>
+        <div className='mt-4 flex flex-wrap items-center justify-center gap-1 text-center text-sm'>
+          <Checkbox className='custom-checkbox' />
+          <span>By using this platform, you agree to our</span>
           <LinkButton type='link' className='pl-0'>
             Disclaimer
           </LinkButton>
         </div>
-
         <div className='mt-6 rounded-lg border-2 border-secondaryBlue bg-white p-4'>
           <div className='text-lg font-bold'>Limited period offer</div>
           <div>
@@ -73,6 +65,11 @@ const Login = () => {
           </div>
         </div>
       </div>
+      <img
+        src='/login_bg_bottom.svg'
+        alt='Background Image'
+        className='absolute -bottom-8 w-full object-cover'
+      />
     </div>
   );
 };
