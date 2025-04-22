177aba69130c4f7442002381e3d2db900c2f4460
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-04-22 17:44:40 +0700

    [Task] 114982 - [330] [Develop] Mobile - Develop UI for Select Plan

--- src/app/(auth)/login/Login.tsx
+++ src/app/(auth)/login/Login.tsx
@@ -29,10 +29,12 @@ const Login = () => {
 
         <div className='mb-6 mt-6 rounded-lg bg-white p-4 shadow-lg shadow-black/20'>
           <div className='flex items-center justify-center'>
-            <h1 className='mb-[4px] mr-[12px] font-bold'>
-              Retrieve Myinfo with
-            </h1>
-            <Image src='/singpass.svg' alt='Logo' width={100} height={100} />
+            <div className='flex flex-row flex-wrap items-center justify-center gap-x-2 gap-y-1 text-center'>
+              <h1 className='whitespace-nowrap font-bold'>
+                Retrieve Myinfo with
+              </h1>
+              <Image src='/singpass.svg' alt='Logo' width={100} height={100} />
+            </div>
           </div>
         </div>
 
@@ -60,12 +62,12 @@ const Login = () => {
         <div className='mt-6 rounded-lg border-2 border-secondaryBlue bg-white p-4'>
           <div className='text-lg font-bold'>Limited period offer</div>
           <div>
-            Flash Sale: Special discount available for the next 50 customers!'
+            Flash Sale: Special discount available for the next 50 customers!
           </div>
           <div className='mt-2 text-base font-bold'>
             15% discount on Car Insurance
           </div>
-          <div className='mt-2 flex w-2/3 items-center justify-between rounded-lg border-2 border-secondaryBlue bg-white p-2 text-left text-secondaryBlue'>
+          <div className='mt-2 flex flex-row items-center gap-2 whitespace-nowrap rounded-lg border-2 border-secondaryBlue bg-white p-2 text-secondaryBlue'>
             <CouponIcon size={32} />
             <div className='text-base font-bold'>Coupon Code CARS15</div>
           </div>
