0ddc485d3fa1e42fdb7d8bd87941f9fc01861976
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-04-23 15:56:40 +0700

    [Task] 115137 - [350] [Develop] Mobile - Develop UI for Review

--- src/components/ProcessBar.tsx
+++ src/components/ProcessBar.tsx
@@ -17,15 +17,25 @@ export default function ProcessBar({ currentStep }: ProcessBarProps) {
   const steps: StepsProps['items'] = stepsData.map(({ title, step }) => {
     const isWaiting = step > currentStep;
 
+    // Check if the title is "Select Add On"
+    const isSelectAddOn = title === 'Select Add On';
+
     return {
       title: (
         <div className='step-title'>
-          {title.split(' ').map((word, i) => (
-            <span key={i} className={i > 0 ? 'new-line' : ''}>
-              {word}
-              {i !== title.split(' ').length - 1 && ' '}
-            </span>
-          ))}
+          {isSelectAddOn ? (
+            <>
+              <span className='new-line'>Select</span>
+              <span className='new-line -mt-[7px]'>Add On</span>
+            </>
+          ) : (
+            title.split(' ').map((word, i) => (
+              <span key={i} className={i > 0 ? 'new-line leading-[1.1]' : ''}>
+                {word}
+                {i !== title.split(' ').length - 1 && ' '}
+              </span>
+            ))
+          )}
         </div>
       ),
       icon: isWaiting ? <div className='custom-step-wait' /> : undefined,
@@ -33,8 +43,14 @@ export default function ProcessBar({ currentStep }: ProcessBarProps) {
   });
 
   return (
-    <div>
-      <Steps current={currentStep} labelPlacement='vertical' items={steps} />
+    <div className='w-full overflow-auto'>
+      <Steps
+        current={currentStep}
+        labelPlacement='vertical'
+        direction='horizontal'
+        responsive={false}
+        items={steps}
+      />
     </div>
   );
 }
