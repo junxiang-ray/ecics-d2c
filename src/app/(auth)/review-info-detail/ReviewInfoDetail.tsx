8d456cac3ea7d7008a1ef61d11b02aabb8c238ad
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-04-25 09:48:35 +0700

    [Task] 115137 - [350] [Develop] Mobile - Develop UI for Review

--- src/app/(auth)/review-info-detail/ReviewInfoDetail.tsx
+++ src/app/(auth)/review-info-detail/ReviewInfoDetail.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { Input } from 'antd';
-import { useState } from 'react';
-import ConfirmInfoModalWrapper from '@/app/(auth)/review-info-detail/modal/ConfirmInfoModalWrapper';
 import Image from 'next/image';
+import { useState } from 'react';
+
 import { PrimaryButton, SecondaryButton } from '@/components/ui/buttons';
+
+import ConfirmInfoModalWrapper from '@/app/(auth)/review-info-detail/modal/ConfirmInfoModalWrapper';
 
 const ReviewInfoDetail = () => {
   const [showConfirmModal, setShowConfirmModal] = useState(false);
@@ -17,79 +19,110 @@ const ReviewInfoDetail = () => {
     setShowConfirmModal(true);
   };
 
+  const boxWrapperClass = 'mt-6 rounded-md border border-gray-300 bg-white p-4';
+
   return (
     <div className='flex min-h-screen flex-col'>
       <div className='relative z-10 flex-grow p-6'>
         <div className='flex items-center justify-between'>
-          <Image src='/ecics.svg' alt='Logo' width={100} height={100} />
           <Image src='/singpass.svg' alt='Logo' width={170} height={170} />
+          <Image src='/ecics.svg' alt='Logo' width={100} height={100} />
         </div>
         <div className='mt-6 text-lg font-bold'>Review your Myinfo details</div>
-        <div className='mt-4'>
-          <div className='text-sm font-bold'>Email Address</div>
-          <Input placeholder='abc@gmail.com' className='mt-2' />
-        </div>
-        <div className='mt-4'>
-          <div className='text-sm font-bold'>Phone Number</div>
-          <Input placeholder='+65 98888888' className='mt-2' />
-        </div>
-        <div className='mt-4'>
-          <div className='text-base font-bold underline underline-offset-4'>
-            Personal Info
-          </div>
-
-          <div className='mt-2 grid grid-cols-2 gap-4'>
-            <div>
-              <div className='text-sm font-bold'>Name as per NRIC</div>
-              <div className='text-sm'>Sayan Chakraborty</div>
+        <div className='w-2/3 justify-self-center'>
+          <div
+            className={`${boxWrapperClass} flex items-center justify-between`}
+          >
+            <div className='w-[calc(50%-10px)]'>
+              <div className='text-sm font-bold'>Email Address</div>
+              <Input placeholder='abc@gmail.com' className='mt-2' />
             </div>
-            <div>
-              <div className='text-sm font-bold'>NRIC</div>
-              <div className='text-sm'>ABC1234</div>
+            <div className='w-[calc(50%-10px)]'>
+              <div className='text-sm font-bold'>Phone Number</div>
+              <Input placeholder='+65 98888888' className='mt-2' />
             </div>
           </div>
 
-          <div className='mt-2 grid grid-cols-2 gap-4'>
-            <div>
-              <div className='text-sm font-bold'>Gender</div>
-              <div className='text-sm'>Male</div>
+          <div className={`${boxWrapperClass} mt-4`}>
+            <div className='text-base font-bold underline underline-offset-4'>
+              Personal Details
             </div>
-            <div>
-              <div className='text-sm font-bold'>Marital Status</div>
-              <div className='text-sm'>Married</div>
+            <div className='mt-2 grid grid-cols-2 gap-4'>
+              <div>
+                <div className='text-sm font-bold'>Name as per NRIC</div>
+                <div className='text-sm'>Sayan Chakraborty</div>
+              </div>
+              <div>
+                <div className='text-sm font-bold'>NRIC</div>
+                <div className='text-sm'>ABC1234</div>
+              </div>
+            </div>
+            <div className='mt-2 grid grid-cols-2 gap-4'>
+              <div>
+                <div className='text-sm font-bold'>Gender</div>
+                <div className='text-sm'>Male</div>
+              </div>
+              <div>
+                <div className='text-sm font-bold'>Marital Status</div>
+                <div className='text-sm'>Married</div>
+              </div>
+            </div>
+            <div className='mt-2 grid grid-cols-2 gap-4'>
+              <div>
+                <div className='text-sm font-bold'>Date of Birth</div>
+                <div className='text-sm'>29/12/1990</div>
+              </div>
+              <div>
+                <div className='text-sm font-bold'>Address</div>
+                <div className='text-sm'>10 Eunos Road Singapore 400087</div>
+              </div>
             </div>
           </div>
 
-          <div className='mt-2 grid grid-cols-2 gap-4'>
-            <div>
-              <div className='text-sm font-bold'>Date of Birth</div>
-              <div className='text-sm'>29/12/1990</div>
+          <div className={`${boxWrapperClass} mt-4`}>
+            <div className='text-base font-bold underline underline-offset-4'>
+              Vehicle Details
             </div>
-            <div>
-              <div className='text-sm font-bold'>Address</div>
-              <div className='text-sm'>10 Eunos Road Singapore 400087</div>
+            <div className='mt-2 grid grid-cols-2 gap-4'>
+              <div>
+                <div className='text-sm font-bold'>Vehicle Make</div>
+                <div className='text-sm'>BMW i5 2.5</div>
+              </div>
+              <div>
+                <div className='text-sm font-bold'>
+                  Vehicle First Registered in
+                </div>
+                <div className='text-sm'>2024</div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className='mt-4'>
-          <div className='text-base font-bold underline underline-offset-4'>
-            Vehicle Details
-          </div>
-          <div className='mt-2 grid grid-cols-2 gap-4'>
-            <div>
-              <div className='text-sm font-bold'>Vehicle Make</div>
-              <div className='text-sm'>BMW i5 2.5</div>
+            <div className='mt-2 grid grid-cols-2 gap-4'>
+              <div>
+                <div className='text-sm font-bold'>
+                  Vehicle Registration Number
+                </div>
+                <div className='text-sm'>SGT1818T</div>
+              </div>
+              <div>
+                <div className='text-sm font-bold'>Chassis Number</div>
+                <div className='text-sm'>234GH3</div>
+              </div>
             </div>
-            <div>
-              <div className='text-sm font-bold'>Year of Registration</div>
+            <div className='mt-2 grid grid-cols-2 gap-4'>
+              <div>
+                <div className='text-sm font-bold'>Engine Number</div>
+                <div className='text-sm'>2345HE3</div>
+              </div>
+              <div>
+                <div className='text-sm font-bold'>Year of Registration</div>
+                <div className='text-sm'>2024</div>
+              </div>
+            </div>
+            <div className='mt-2'>
+              <div className='text-sm font-bold'>
+                Driving Licence - Qualified Driving License Validity
+              </div>
               <div className='text-sm'>2024</div>
             </div>
-          </div>
-
-          <div className='mt-2'>
-            <div className='text-sm font-bold'>Chassis Number</div>
-            <div className='text-sm'>SGT1818T</div>
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,8 @@ const config: Config = {
         'brand-blue': '#00ADEF',
         'plan-blue': '#b6dbfd',
         'review-blue': '#F4FBFD',
+        'coupon-blue': '#0095CE',
+        'red-logo': '#F4333D',
       },
       animation: {
         'slide-up': 'slide-up 0.3s ease-out',
