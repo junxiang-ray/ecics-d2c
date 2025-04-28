ce11e90db0b194aa584f5ae7251f09d25ba34b86
lq <lerkqing_chiew@ecics.com.sg>
2025-04-28 16:39:28 +0800

    [Task] 115259- [320] [Develop] Mobile - Integrate API for Personal & Vehicle Details

--- src/app/insurance/basic-detail/page.tsx
+++ src/app/insurance/basic-detail/page.tsx
@@ -1,9 +1,30 @@
+import { VehicleSelection } from '@/components/VehicleSelection';
 import { PolicyDetail } from './PolicyDetail';
 
 export default function PolicyDetailPage() {
+  // list from Singpass
+  // const veh_selection_option = [
+  //   { value: 'BMW | 116d 1.5', text: 'BMW 116d 1.5' },
+  //   {
+  //     value: 'MERCEDES BENZ | A200 AMG Line 1.4',
+  //     text: 'MERCEDES BENZ A200 AMG Line 1.4',
+  //   },
+  // ];
+
+  //selected vehicle from singpass page
+  const selected_vehicle_singpass: VehicleSelection = {
+    regNo: 'SPT1818T',
+    make: 'BMW',
+    model: '116d 1.5',
+    first_registered_year: 2024,
+  };
+
   return (
     <div>
-      <PolicyDetail />
+      <PolicyDetail
+        isSingPassFlow={true}
+        selected_vehicle_singpass={selected_vehicle_singpass}
+      />
     </div>
   );
 }
