87cf8e04a5ed7c82f327d913e1cbb5f01711abdd
TDT-Nathan Khong <chinhkhong@cqtdt.asia>
2025-05-07 22:06:24 +0700

    fix comment

--- src/app/insurance/components/AdditionDriver.tsx
+++ src/app/insurance/components/AdditionDriver.tsx
@@ -1,8 +1,14 @@
 import DeleteIcon from '@/components/icons/DeleteIcon';
-import { useDeviceDetection } from '@/hook/useDeviceDetection';
+import { DatePickerField } from '@/components/ui/form/datepicker';
+import { DropdownField } from '@/components/ui/form/dropdownfield';
+import { InputField } from '@/components/ui/form/inputfield';
 import { PlusOutlined } from '@ant-design/icons';
-import { DatePicker, Drawer, Form, Input, Modal, Select } from 'antd';
-import { useRef, useState } from 'react';
+import { Drawer } from 'antd';
+import { zodResolver } from '@hookform/resolvers/zod';
+import dayjs from 'dayjs';
+import { useMemo, useRef, useState } from 'react';
+import { FormProvider, useForm, useFieldArray } from 'react-hook-form';
+import { z } from 'zod';
 
 interface Props {
   isShowAdditionDriver: boolean;
@@ -10,61 +16,90 @@ interface Props {
   setDataDrivers: (data: any[]) => void;
 }
 
-const SELECT_OPTIONS = {
-  gender: [
-    { value: 'male', label: 'Male' },
-    { value: 'female', label: 'Female' },
-  ],
-  maritalStatus: [
-    { value: 'single', label: 'Single' },
-    { value: 'married', label: 'Married' },
-    { value: 'divorced', label: 'Divorced' },
-    { value: 'widowed', label: 'Widowed' },
-  ],
-  driving: [
-    { value: 'Less than 1 year', label: 'Less than 1 year' },
-    { value: '1 year', label: '1 year' },
-    { value: '2 year', label: '2 years' },
-    { value: '3 year', label: '3 years' },
-    { value: '4 year', label: '4 years' },
-    { value: '5 year', label: '5 years' },
-    { value: '6 year', label: '6 years and above' },
-  ],
-};
+export const GENDER_OPTIONS = [
+  { value: 'male', text: 'Male' },
+  { value: 'female', text: 'Female' },
+];
 
-const AdditionDriver = (props: Props) => {
-  const { isShowAdditionDriver, setIsShowAdditionDriver, setDataDrivers } =
-    props;
+export const MARITAL_STATUS_OPTIONS = [
+  { value: 'single', text: 'Single' },
+  { value: 'married', text: 'Married' },
+  { value: 'divorced', text: 'Divorced' },
+  { value: 'widowed', text: 'Widowed' },
+];
 
-  const isMobile = useDeviceDetection();
-  const ref = useRef<(HTMLDivElement | null)[]>([]);
-  const [drivers, setDrivers] = useState([1]);
+export const DRIVING_EXPERIENCE_OPTIONS = [
+  { value: 'Less than 1 year', text: 'Less than 1 year' },
+  { value: '1 year', text: '1 year' },
+  { value: '2 year', text: '2 years' },
+  { value: '3 year', text: '3 years' },
+  { value: '4 year', text: '4 years' },
+  { value: '5 year', text: '5 years' },
+  { value: '6 year', text: '6 years and above' },
+];
+
+const createSchema = () =>
+  z.object({
+    drivers: z
+      .array(
+        z.object({
+          name: z.string().min(1, 'Name is required'),
+          nric: z.string().min(1, 'NRIC/FIN is required'),
+          birthDate: z
+            .date({ required_error: 'Date of birth is required' })
+            .refine(
+              (date) => date <= new Date(),
+              'Date of birth cannot be in the future',
+            ),
+          gender: z.string().min(1, 'Gender is required'),
+          marital: z.string().min(1, 'Marital status is required'),
+          Driving: z.string().min(1, 'Driving experience is required'),
+        }),
+      )
+      .min(1, 'At least one driver is required'),
+  });
+
+type FormData = z.infer<ReturnType<typeof createSchema>>;
+
+const AdditionDriver = ({
+  isShowAdditionDriver,
+  setIsShowAdditionDriver,
+  setDataDrivers,
+}: Props) => {
+  const schema = useMemo(() => createSchema(), []);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const methods = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: { drivers: [{} as any] },
+    mode: 'onSubmit',
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'drivers',
+  });
+
   const [isWarningDriver, setIsWarningDriver] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-  const [form] = Form.useForm();
 
   const handleAddDriver = () => {
-    if (drivers.length < 3) {
-      const newDriverIndex = drivers.length;
-      const newDrivers = [newDriverIndex + 1, ...drivers];
-      setDrivers(newDrivers);
-
+    if (fields.length < 3) {
+      append({} as any);
       setTimeout(() => {
-        const lastDriverEl = ref.current[newDrivers.length - 1];
-        if (lastDriverEl) {
-          lastDriverEl.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
+        scrollRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
       }, 100);
     }
   };
 
   const handleRemoveDriver = (index: number) => {
-    const updatedDrivers = drivers.filter((_, i) => i !== index);
-    setDrivers(updatedDrivers);
-    const currentDrivers = form.getFieldValue('drivers') || [];
-    form.setFieldsValue({
-      drivers: currentDrivers.filter((_: any, i: number) => i !== index),
-    });
+    remove(index);
   };
 
   const onChangeDriver = (value: string) => {
@@ -77,126 +112,79 @@ const AdditionDriver = (props: Props) => {
     }
   };
 
-  const onFinish = (values: any) => {
-    console.log('value', values);
-    setDataDrivers(values.drivers);
+  const onSubmit = (data: FormData) => {
+    setDataDrivers(data.drivers);
     setIsShowAdditionDriver(false);
   };
 
-  const renderInput = (
-    label: string,
-    maxLength: number,
-    name: (string | number)[],
-  ) => (
-    <div className='flex flex-col gap-2'>
-      <p className='text-base font-semibold text-[#1E1E1E]'>{label}</p>
-      <Form.Item
-        name={name}
-        rules={[
-          { required: true, message: `${label} is required` },
-          { min: 3, message: `${label} must be at least 3 characters` },
-          {
-            max: maxLength,
-            message: `${label} cannot exceed ${maxLength} characters`,
-          },
-        ]}
-      >
-        <Input
-          className='h-[39px] w-full rounded-lg border border-[#81899466] px-4 text-sm font-normal text-[#1E1E1E]'
-          maxLength={maxLength}
-        />
-      </Form.Item>
-    </div>
-  );
+  const _renderFormInput = () => {
+    return (
+      <>
+        {fields.map((field, index) => (
+          <div
+            key={field.id}
+            className='flex w-full flex-col gap-3 rounded-lg border border-[#E5E5E5] bg-[#8189940F] px-4 py-2'
+          >
+            <div className='flex w-full flex-row items-center justify-between'>
+              <p className='text-xl font-semibold text-[#080808]'>
+                Additional Driver {index + 1}
+              </p>
+              <div className='mt-3'>
+                <DeleteIcon
+                  className='cursor-pointer text-red-500'
+                  onClick={() => handleRemoveDriver(index)}
+                />
+              </div>
+            </div>
 
-  const renderInputDate = (label: string, name: (string | number)[]) => (
-    <div className='flex flex-col gap-2'>
-      <p className='text-base font-semibold text-[#1E1E1E]'>{label}</p>
-      <Form.Item
-        name={name}
-        rules={[{ required: true, message: `${label} is required` }]}
-      >
-        <DatePicker
-          className='h-[39px] w-full rounded-lg border border-[#81899466] text-sm font-normal text-[#1E1E1E]'
-          format='DD/MM/YYYY'
-        />
-      </Form.Item>
-    </div>
-  );
+            <InputField
+              name={`drivers.${index}.name`}
+              label='Name as Per NRIC'
+              placeholder='Enter your Name'
+            />
+            <InputField
+              name={`drivers.${index}.nric`}
+              label='NRIC'
+              placeholder='Enter NRIC/FIN'
+            />
+            <DatePickerField
+              name={`drivers.${index}.birthDate`}
+              label='Date of Birth'
+            />
+            <DropdownField
+              name={`drivers.${index}.gender`}
+              label='Gender'
+              placeholder='Select gender'
+              options={GENDER_OPTIONS}
+            />
+            <DropdownField
+              name={`drivers.${index}.marital`}
+              label='Marital Status'
+              placeholder='Select marital status'
+              options={MARITAL_STATUS_OPTIONS}
+            />
+            <DropdownField
+              name={`drivers.${index}.Driving`}
+              label='Driving Experience'
+              placeholder='Select driving experience'
+              options={DRIVING_EXPERIENCE_OPTIONS}
+              onChange={onChangeDriver}
+            />
 
-  const renderSelect = (
-    label: string,
-    name: (string | number)[],
-    options: { value: string; label: string }[],
-    onChange?: (value: string) => void,
-    status?: boolean,
-    errorMessage?: string,
-  ) => (
-    <div className='flex flex-col gap-2'>
-      <p className='text-base font-semibold text-[#1E1E1E]'>{label}</p>
-      <Form.Item
-        name={name}
-        rules={[{ required: true, message: `${label} is required` }]}
-        validateStatus={status ? 'error' : undefined}
-        help={errorMessage}
-      >
-        <Select
-          className={`h-[39px] w-full rounded-lg border ${status ? 'border-red' : 'border-[#81899466]'} text-sm font-normal text-[#1E1E1E]`}
-          options={options}
-          onChange={onChange}
-        />
-      </Form.Item>
-    </div>
-  );
-
-  const renderDriver = (index: number) => (
-    <div
-      key={index}
-      className='flex w-full flex-col gap-4 rounded-lg border border-[#E5E5E5] bg-[#8189940F] px-4 py-2'
-      ref={(el) => {
-        ref.current[index] = el;
-      }}
-    >
-      <div className='flex w-full flex-row items-center justify-between'>
-        <p className='text-xl font-semibold leading-8 text-[#080808]'>
-          Additional Driver {index + 1}
-        </p>
-        <div className='mt-3'>
-          <DeleteIcon
-            className='cursor-pointer text-red-500'
-            onClick={() => handleRemoveDriver(index)}
-          />
-        </div>
-      </div>
-      {renderInput('Name as Per NRIC', 60, ['drivers', index, 'driverName'])}
-      {renderInput('NRIC', 9, ['drivers', index, 'nric'])}
-      {renderInputDate('Date of Birth', ['drivers', index, 'birthDate'])}
-      {renderSelect(
-        'Gender',
-        ['drivers', index, 'gender'],
-        SELECT_OPTIONS.gender,
-      )}
-      {renderSelect(
-        'Marital Status',
-        ['drivers', index, 'marital'],
-        SELECT_OPTIONS.maritalStatus,
-      )}
-      {renderSelect(
-        'Driving Experience',
-        ['drivers', index, 'Driving'],
-        SELECT_OPTIONS.driving,
-        onChangeDriver,
-        isWarningDriver,
-        errorMessage,
-      )}
-    </div>
-  );
+            {isWarningDriver && (
+              <p className='text-sm text-red-500'>{errorMessage}</p>
+            )}
+          </div>
+        ))}
+      </>
+    );
+  };
 
   const content = (
-    <div className='flex flex-col gap-8'>
-      {drivers.map((_, index) => renderDriver(index))}
-      {drivers.length < 3 && (
-        <div className='flex flex-row justify-end'>
+    <>
+      {_renderFormInput()}
+      {fields.length < 3 && (
+        <div className='flex flex-row justify-end' ref={scrollRef}>
           <button
             className='flex flex-row items-center gap-2 rounded-md border border-[#00ADEF] px-4 py-2 text-sm font-normal text-[#00ADEF]'
             onClick={handleAddDriver}
@@ -207,7 +195,6 @@ const AdditionDriver = (props: Props) => {
           </button>
         </div>
       )}
-
       <div className='flex w-full flex-row justify-between gap-4 rounded-md bg-[#DCDDDC4F] px-8 py-2 text-base font-bold leading-[21px]'>
         <button
           className='rounded-md border border-[#0096D8] bg-white px-8 py-2 text-[#00ADEF]'
@@ -223,10 +210,10 @@ const AdditionDriver = (props: Props) => {
           Save
         </button>
       </div>
-    </div>
+    </>
   );
 
-  return isMobile.isMobile ? (
+  return (
     <Drawer
       placement='bottom'
       open={isShowAdditionDriver}
@@ -235,29 +222,19 @@ const AdditionDriver = (props: Props) => {
       className='w-full rounded-t-xl'
       onClose={() => setIsShowAdditionDriver(false)}
     >
-      <div style={{ maxHeight: '90vh', overflowY: 'auto' }}>
-        <Form form={form} onFinish={onFinish} initialValues={{ drivers: [] }}>
-          {content}
-        </Form>
+      <div
+        style={{ maxHeight: '90vh', overflowY: 'auto', overflowX: 'hidden' }}
+      >
+        <FormProvider {...methods}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='flex flex-col gap-8 py-6'
+          >
+            {content}
+          </form>
+        </FormProvider>
       </div>
     </Drawer>
-  ) : (
-    <Modal
-      open={isShowAdditionDriver}
-      closable={false}
-      onCancel={() => setIsShowAdditionDriver(false)}
-      maskClosable={false}
-      keyboard={false}
-      footer={null}
-      centered
-      width={600}
-    >
-      <div style={{ maxHeight: 'calc(100vh - 60px)', overflowY: 'auto' }}>
-        <Form form={form} onFinish={onFinish}>
-          {content}
-        </Form>
-      </div>
-    </Modal>
   );
 };
 
