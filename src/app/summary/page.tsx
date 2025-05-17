65a2980054efc0e45a827cb61645e9f5570666f8
TDT-Nathan Khong <chinhkhong@cqtdt.asia>
2025-05-17 18:07:13 +0700

    remove conssole.log

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -122,7 +122,7 @@ export default function Summary() {
       </div>
     );
   }
-  console.log(quote?.product_type.documents, 'chinh123');
+
   return (
     <div className='flex w-full justify-center '>
       <div className='w-full max-w-[1280px]'>
