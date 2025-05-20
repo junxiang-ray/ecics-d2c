a1f1f7cc8cd68c27b921f9c1821705b91ada8fd3
TDT-Nathan Khong <chinhkhong@cqtdt.asia>
2025-05-20 13:57:28 +0700

    fix show content screen addon and review

--- src/app/motor/insurance/add-on/AddonDetail.tsx
+++ src/app/motor/insurance/add-on/AddonDetail.tsx
@@ -414,7 +414,7 @@ function AddOnDetail({
 
   return (
     <div className='flex w-full flex-col items-center'>
-      <div className='flex w-full max-w-[1280px] flex-col items-center justify-center md:mb-24'>
+      <div className='flex w-full max-w-[1280px] flex-col items-center justify-center pb-6 md:mb-24 md:pb-2'>
         {isShowBonusDetail ? (
           <AddOnBonusDetailManualForm
             planFee={premiumWithGst}
