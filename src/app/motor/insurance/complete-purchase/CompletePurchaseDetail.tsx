144e2b3b3d1e0baa6407a31310753a512dda6ed6
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-06-09 10:38:47 +0700

    [Task] 117402 - [CR] [Car] [Summary] Update UI for Payment Success screen

--- src/app/motor/insurance/complete-purchase/CompletePurchaseDetail.tsx
+++ src/app/motor/insurance/complete-purchase/CompletePurchaseDetail.tsx
@@ -177,7 +177,10 @@ export default function CompletePurchaseDetail({
     (plan) => plan.title && plan.title.includes(selectedPlanTitle),
   );
   const addonsTitles =
-    matchedPlan?.benefits?.map((benefit) => benefit.name).filter(Boolean) || [];
+    matchedPlan?.benefits
+      ?.filter((benefit) => benefit.is_active)
+      .map((benefit) => benefit.name)
+      .filter(Boolean) || [];
 
   const sharedDataMap: {
     [key: string]: { title: string; value: any; coverage_amount?: string }[];
@@ -674,7 +677,6 @@ export default function CompletePurchaseDetail({
                 sections[index + 1]?.key === 'addons'
               ) {
                 const policyData = sharedDataMap['policy_plan'] || [];
-                console.log('policyData', policyData);
                 const addonsData = sharedDataMap['addons'] || [];
 
                 return (
