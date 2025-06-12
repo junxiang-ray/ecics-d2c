5999d2da2b3d1ce65bfb2a5a805a16bbea6f9e26
TDT-Nathan Khong <chinhkhong@cqtdt.asia>
2025-06-12 17:16:23 +0700

    Task -117291-[540] [Develop] Desktop and Mobile - Integrate API for Select Plan

--- src/api/base-service/maid.ts
+++ src/api/base-service/maid.ts
@@ -1,6 +1,4 @@
-import { SavePersonalInfoPayload } from '@/libs/types/auth';
-
-import { API_GET_QUOTE_BY_KEY } from '@/constants/api.constant';
+import { API_GET_QUOTE_BY_KEY, API_SAVE_QUOTE } from '@/constants/api.constant';
 
 import baseClient from './api.config';
 import {
@@ -16,30 +14,12 @@ export default {
   generateMaidQuote(data: MaidQuoteCreationPayload) {
     return baseClient.post<MaidQuoteResponse>('/maid/quote', data);
   },
-  //   saveProposal(data: ProposalPayload) {
-  //     return baseClient.post<any>('/car/proposal', data);
-  //   },
-  //   saveQuote(key: string, data: any, is_sending_email: boolean) {
-  //     const formatData = {
-  //       data: { ...data },
-  //       key: key,
-  //       is_sending_email: is_sending_email,
-  //     };
-  //     return baseClient.post<QuoteResponse>(`${API_SAVE_QUOTE}`, formatData);
-  //   },
-  //   postPersonalInfoSave(payload: SavePersonalInfoPayload) {
-  //     return baseClient.post<any>(`${API_POST_PERSONAL_INFO_SAVE}`, payload);
-  //   },
-  //   requestLogCar() {
-  //     return baseClient.get<any>('/request-log/car');
-  //   },
-  //   getHirePurchaseList(product_type: string) {
-  //     return baseClient.get<any>('/companies/' + product_type);
-  //   },
-  //   saveProposalFinalize(data: { key: string }) {
-  //     return baseClient.post<any>('/car/proposal/finalize', data);
-  //   },
-  //   payment(data: { key: string }) {
-  //     return baseClient.post<any>('/payment', data);
-  //   },
+  saveMaidQuote(key: string, data: any, is_sending_email: boolean) {
+    const formatData = {
+      data: { ...data },
+      key: key,
+      is_sending_email: is_sending_email,
+    };
+    return baseClient.post<MaidQuoteResponse>(`${API_SAVE_QUOTE}`, formatData);
+  },
 };
