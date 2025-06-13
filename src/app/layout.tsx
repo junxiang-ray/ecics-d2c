481b855c525bdddaf668f021d88f4100f285ba8a
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-06-13 09:10:09 +0700

    [Task] 118284 - Request to Add Microsoft Clarity Script to header tag on the Website v1 & v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <title>ECICS Insurance</title>
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rntrxo7r12");`,
+          }}
+        />
+      </head>
       <body className={openSans.className}>
         <Suspense fallback={null}>
           <ReduxProvider>
