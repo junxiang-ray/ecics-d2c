309286c9c0bcd2f1c0be0721052a90bd0edb5024
TDT-Nathan Khong <chinhkhong@cqtdt.asia>
2025-06-19 14:37:50 +0700

    update regex email

--- src/app/maid/insurance/basic-detail/PolicyDetailForm.tsx
+++ src/app/maid/insurance/basic-detail/PolicyDetailForm.tsx
@@ -36,7 +36,6 @@ import { PromoCodeField } from '@/app/motor/insurance/components/PromoCode';
 import { MAID_QUOTE } from '@/constants';
 import { emailRegex, phoneRegex } from '@/constants/validation.constant';
 import { useGetNationality } from '@/hook/insurance/common';
-import { REGEX_TEXT } from '@/app/api/utils/regex';
 
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
@@ -338,13 +337,12 @@ const PolicyDetailForm = ({
                             onChange={(
                               e: React.ChangeEvent<HTMLInputElement>,
                             ) => {
-                              const value = e.target.value.replace(
-                                REGEX_TEXT,
-                                '',
-                              );
-                              methods.setValue(MAID_QUOTE.email, value, {
-                                shouldValidate: true,
-                              });
+                              const value = e.target.value;
+                              if (emailRegex.test(value) || value === '') {
+                                methods.setValue(MAID_QUOTE.email, value, {
+                                  shouldValidate: true,
+                                });
+                              }
                             }}
                           />
                         </Form.Item>
