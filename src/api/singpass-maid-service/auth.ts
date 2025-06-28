d75fc678b895c1f347eec066f4b4ef2adfae478e
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-06-28 14:36:10 +0700

    [Task] 118993 - [300] [Car] Update the UI for Singpass MyInfo Retrieval screen

--- src/api/singpass-maid-service/auth.ts
+++ src/api/singpass-maid-service/auth.ts
@@ -1,6 +1,6 @@
-import { UserInfoPayload } from '@/libs/types/auth';
+import { LoginResponse, UserInfoPayload } from '@/libs/types/auth';
 
-import { API_GET_USER_INFO } from '@/constants/api.constant';
+import { API_GET_USER_INFO, API_LOGIN } from '@/constants/api.constant';
 
 import singpassMaidService from './api.config';
 
