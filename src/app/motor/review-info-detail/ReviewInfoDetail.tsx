1828cfacde13202370555794d4eae69a68befa48
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-07-11 09:10:46 +0700

    [Bug] 119939 - [360] [Car] [Bug] Redirect wrong screen and back button didn't show the pop-up

--- src/app/motor/review-info-detail/ReviewInfoDetail.tsx
+++ src/app/motor/review-info-detail/ReviewInfoDetail.tsx
@@ -321,15 +321,6 @@ const ReviewInfoDetail = () => {
           'The listed driver has less than 2 years of driving experience.',
       });
     }
-    // Check vehicle number
-    const vehicleNumber = userInfoCar?.vehicles?.find(
-      (v: any) => v?.vehicleno?.value,
-    );
-
-    if (!vehicleNumber?.vehicleno?.value) {
-      setShowVehicleNotFoundModal(true);
-      return;
-    }
     // Call api savePersonalInfo
     callApiPersonalInfo();
   };
