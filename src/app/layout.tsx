e60033b0f353ebd5ef7cdc6949af68eedfaed1a8
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-07-23 15:53:26 +0700

    [Task] 120902 - [Task] [Car] [Maid] Remove the pop-up "Do you want to leave this site" for Car and Maid (Singpass and Manual)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
           <ReduxProvider>
             <ReactQueryProvider>
               <AntdRegistry>
-                <NavigationConfirmProvider />
+                {/*<NavigationConfirmProvider />*/}
                 {children}
               </AntdRegistry>
             </ReactQueryProvider>
