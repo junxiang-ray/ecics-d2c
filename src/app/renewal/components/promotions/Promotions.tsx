9197bf5f85169b8df3d9f08154890b6f2724d4c6
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-08-11 17:04:21 +0700

    Setup structure

---
+++ src/app/renewal/components/promotions/Promotions.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const Promotions = () => {
+  return <div className=''></div>;
+};
+
+export default Promotions;
--- src/app/renewal/page.tsx
+++ src/app/renewal/page.tsx
@@ -1,24 +1,28 @@
-// import RenewalList from './components/RenewalList';
-// import Promotions from './components/Promotions';
-// import Announcements from './components/Announcements';
-// import QuickActions from './components/QuickActions';
+import Announcements from '@/app/renewal/components/announcements/Announcements';
+import Promotions from '@/app/renewal/components/promotions/Promotions';
+import QuickActions from '@/app/renewal/components/quick-action/QuickActions';
+import PoliciesPendingRenewal from '@/app/renewal/components/policies-renewal/PoliciesPendingRenewal';
+import RenewalHeader from '@/app/renewal/components/renewal-header/RenewalHeader';
 
 export default function RenewalPage() {
   return (
     <main className='p-6'>
+      <RenewalHeader />
       <h1 className='text-2xl font-bold'>Welcome back, John</h1>
       <p className='mb-6 text-gray-500'>
         Manage your policies and stay protected
       </p>
 
-      <section className='mb-8'>{/*<RenewalList />*/}</section>
+      <section className='mb-8'>
+        <PoliciesPendingRenewal />
+      </section>
 
       <div className='mb-8 grid gap-6 md:grid-cols-2'>
-        {/*<Promotions />*/}
-        {/*<Announcements />*/}
+        <Promotions />
+        <Announcements />
       </div>
 
-      {/*<QuickActions />*/}
+      <QuickActions />
     </main>
   );
 }
