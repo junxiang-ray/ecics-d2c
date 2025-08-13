87dee8e8756929d140aa8aab185f2240d14f2d16
TDT-Nathan Khong <chinhkhong@cqtdt.asia>
2025-08-13 09:13:30 +0700

    [Renewal] [Desktop] Create Business Partner Bar and profile

---
+++ src/app/renewal/settings/SettingRenewalPage.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { useRouter } from 'next/navigation';
+
+export default function SettingRenewalPage() {
+  const router = useRouter();
+
+  return (
+    <div className='flex w-full flex-col gap-4'>
+      <div className='relative flex h-[56px] w-full flex-row items-center justify-center border-b border-gray-200'>
+        <div
+          className='absolute left-4 cursor-pointer md:left-14'
+          onClick={() => {
+            router.push('/renewal');
+          }}
+        >
+          <ArrowLeftOutlined />
+        </div>
+        <p className='font-semibold'>Settings</p>
+      </div>
+      <div className='flex w-full flex-col justify-center p-3'>
+        <div></div>
+      </div>
+    </div>
+  );
+}
