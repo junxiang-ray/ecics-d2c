e02210882f25b207d25d3f0d2e9cd5390676f9cd
Do Ngoc Son <sondo1@cqtdt.asia>
2025-08-14 16:34:26 +0700

    Apply new logic for validate Vehicle Make & Vehicle Model

---
+++ src/app/api/configs/api-check-vehicle.config.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: process.env.API_DETECT_MAKE_MODEL_URL || '',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${process.env.API_TOKEN_DETECT_MAKE_MODEL || ''}`,
+  },
+});
