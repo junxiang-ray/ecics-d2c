7a459026fa69b9168a3eb7e5160e7acc651a5a2e
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-08-26 14:58:12 +0700

    [Bug] 122565 - [Bug] Comprehensive Plan description at Summary screen turns into Family NCD description on Production

--- src/app/maid/insurance/complete-purchase/CompletePurchaseDetail.tsx
+++ src/app/maid/insurance/complete-purchase/CompletePurchaseDetail.tsx
@@ -322,10 +322,10 @@ export default function CompletePurchaseDetail({
   }));
 
   const selectedPlanTitle = maidQuote?.data?.selected_plan || 'N/A';
+  const selectedPlanCode = maidQuote?.data?.selected_plan_code;
   const plans = maidQuote?.data?.plans || [];
-  const matchedPlan = plans.find(
-    (plan) => plan.title && plan.title.includes(selectedPlanTitle),
-  );
+  const matchedPlan = plans.find((plan) => plan.code === selectedPlanCode);
+
   const addonsTitles = matchedPlan?.benefits || [];
 
   const sharedDataMap: {
