2bf2a7dc3a4e38be4b06ea773a499fe037b3baeb
M-Raas <224732628+M-Raas@users.noreply.github.com>
2025-09-17 17:18:57 +0800

    setting up for vercel preview deployment

--- src/api/singpass-maid-service/api.config.ts
+++ src/api/singpass-maid-service/api.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const singpassMaidService = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '/api/v1',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
