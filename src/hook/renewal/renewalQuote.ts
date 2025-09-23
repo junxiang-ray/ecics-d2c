c3c9331cdca831030908674d6d75da3b2b20f0eb
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-09-23 17:21:03 +0700

    [Task] 123727 - [Renewal] Update the price of add-ons at Policy Renewal screen when users choose different Renewal Expiry Date

--- src/hook/renewal/renewalQuote.ts
+++ src/hook/renewal/renewalQuote.ts
@@ -34,18 +34,10 @@ export const usePostEditRenewal = (mergeStrategy: MergeStrategy = 'basic') => {
         ...renewalQuote,
         renewal_info: {
           ...renewalQuote.renewal_info,
-          renewalplanprem:
-            apiData.renewal_info?.renewalplanprem ??
-            renewalQuote.renewal_info?.renewalplanprem,
-          renewalpremb4gst:
-            apiData.renewal_info?.renewalpremb4gst ??
-            renewalQuote.renewal_info?.renewalpremb4gst,
-          renewalgst:
-            apiData.renewal_info?.renewalgst ??
-            renewalQuote.renewal_info?.renewalgst,
-          renewalpremwgst:
-            apiData.renewal_info?.renewalpremwgst ??
-            renewalQuote.renewal_info?.renewalpremwgst,
+          renewalplanprem: apiData.renewal_info?.renewalplanprem,
+          renewalpremb4gst: apiData.renewal_info?.renewalpremb4gst,
+          renewalgst: apiData.renewal_info?.renewalgst,
+          renewalpremwgst: apiData.renewal_info?.renewalpremwgst,
         },
       };
 
