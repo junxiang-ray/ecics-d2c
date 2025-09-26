af21177e653ad4ba6e0da1cd6ecd1efe4c0a1d20
ECO1292-HUYVQ <huyvu@cqtdt.asia>
2025-09-26 16:06:46 +0700

    [Task] 123924 - [Renewal] colors of icon background

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -35,7 +35,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['app', 'general', 'renewalQuote'],
+  whitelist: ['app', 'general', 'renewalQuote', 'ecicsUserInfo'],
 };
 
 // Persisted reducer
