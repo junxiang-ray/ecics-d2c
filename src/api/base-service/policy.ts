ff002fe2827787e614dbfe97d20e0f11e4cc1c10
ducle <ducle@cqtdt.asia>
2025-11-26 14:49:55 +0700

    fix type error"

--- src/api/base-service/policy.ts
+++ src/api/base-service/policy.ts
@@ -856,7 +856,7 @@ export default {
             data: {
               data: getPolicy(payload),
               meta: { pagination: {} },
-            } as any,
+            } as unknown as AxiosResponse<T>,
           });
         },
         Math.ceil(Math.random() * 100 + 50),
