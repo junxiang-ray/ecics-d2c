5c12828fbc089b329c0745eaedc6a75df0c3a133
ducle <ducle@cqtdt.asia>
2025-11-27 14:50:46 +0700

    Fix lint error

--- src/app/portal/claims/layout.tsx
+++ src/app/portal/claims/layout.tsx
@@ -2,6 +2,10 @@
 
 import ClaimProvider from '@/components/layouts/ClaimLayout';
 
+interface Props {
+  children: JSX.Element;
+}
+
 const Layout = ({ children }: Props): JSX.Element => {
   return <ClaimProvider>{children}</ClaimProvider>;
 };
