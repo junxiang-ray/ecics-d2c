5c7b901ec3a7e5d9fea7a395720edee082c68afd
ducle <ducle@cqtdt.asia>
2025-12-04 14:20:05 +0700

    Fix lint

--- src/components/page/user-profile/components/FormItem.tsx
+++ src/components/page/user-profile/components/FormItem.tsx
@@ -78,7 +78,9 @@ const FormItem = <FormValues,>({
                     size='large'
                     onChange={(event) => {
                       const value = event.target.value;
-                      const formatted = formatter ? formatter(value) : value;
+                      const formatted = (formatter
+                        ? formatter(value)
+                        : value) as unknown as FormValues[Props<FormValues>['name']];
                       field.onChange(formatted);
                     }}
                   />
