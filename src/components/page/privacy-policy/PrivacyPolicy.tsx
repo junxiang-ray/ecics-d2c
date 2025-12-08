e7105f1f7b5d58a0de708a387f30a4f8f0bff2ac
ducle <ducle@cqtdt.asia>
2025-12-08 10:35:20 +0700

    Fix - Click back button not trigger anything

--- src/components/page/privacy-policy/PrivacyPolicy.tsx
+++ src/components/page/privacy-policy/PrivacyPolicy.tsx
@@ -4,14 +4,16 @@ import { PrivacyPolicyAttr } from '@/libs/types/privacy-policy';
 
 import { formatDateString } from '@/libs/utils/dayjs';
 
+import { Button } from 'antd';
 import BackOutlined from '@/assets/icons/renewal/back.svg';
 
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { usePrivacyPolicies } from '@/hook/privacy-policy/privacy-policy';
 
 const PrivacyPolicy = (): JSX.Element => {
+  const router = useRouter();
   const { data, isFetching } = usePrivacyPolicies();
 
   const isNodata: boolean = !isFetching && !data;
@@ -21,13 +23,17 @@ const PrivacyPolicy = (): JSX.Element => {
   return (
     <>
       <div className='mb-3'>
-        <button
-          className='-ml-2 flex flex-nowrap items-start gap-4'
-          aria-label='Back'
-        >
-          <span className='rou:nded-md inline-block p-2 hover:bg-gray-100'>
+        <div className='flex flex-nowrap items-start gap-4'>
+          <Button
+            className='px-2 text-black'
+            color='default'
+            type='link'
+            size='large'
+            title='Back'
+            onClick={() => router.back()}
+          >
             <BackOutlined />
-          </span>
+          </Button>
           <div className='mb-2 flex flex-col items-start gap-3'>
             <div className='font-heading text-3xl font-bold text-gray-900'>
               Privacy Policy
@@ -36,7 +42,7 @@ const PrivacyPolicy = (): JSX.Element => {
               Learn how we protect and manage your personal information
             </p>
           </div>
-        </button>
+        </div>
       </div>
       <div className='rounded-lg border border-gray-200 bg-white p-6 shadow-sm'>
         <div className='prose prose-gray'>
--- src/components/page/term/TermAndConditions.tsx
+++ src/components/page/term/TermAndConditions.tsx
@@ -1,16 +1,19 @@
 'use client';
+
 import { TermAndConditionAttr } from '@/libs/types/term-and-condition';
 
 import { formatDateString } from '@/libs/utils/dayjs';
 
+import { Button } from 'antd';
 import BackOutlined from '@/assets/icons/renewal/back.svg';
 
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useTermAndConditions } from '@/hook/term-and-condition/term-and-condition';
 
 const TermAndConditions = (): JSX.Element => {
+  const router = useRouter();
   const { data, isFetching } = useTermAndConditions();
 
   const isNodata: boolean = !isFetching && !data;
@@ -20,13 +23,17 @@ const TermAndConditions = (): JSX.Element => {
   return (
     <>
       <div className='mb-3'>
-        <button
-          className='-ml-2 flex flex-nowrap items-start gap-4'
-          aria-label='Back'
-        >
-          <span className='rou:nded-md inline-block p-2 hover:bg-gray-100'>
+        <div className='flex flex-nowrap items-start gap-4'>
+          <Button
+            className='px-2 text-black'
+            color='default'
+            type='link'
+            size='large'
+            title='Back'
+            onClick={() => router.back()}
+          >
             <BackOutlined />
-          </span>
+          </Button>
           <div className='mb-2 flex flex-col items-start gap-3'>
             <div className='font-heading text-3xl font-bold text-gray-900'>
               Terms and Conditions
@@ -35,7 +42,7 @@ const TermAndConditions = (): JSX.Element => {
               Please read our terms and conditions carefully
             </p>
           </div>
-        </button>
+        </div>
       </div>
       <div className='rounded-lg border border-gray-200 bg-white p-6 shadow-sm'>
         <div className='prose prose-gray'>
